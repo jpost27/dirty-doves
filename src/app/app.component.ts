80551a439d8c5ff0a8e8d7f224beae5150adc7dc
Josh Post <joshpost94@gmail.com>
2023-02-02 18:30:42 -0500

    Revert "moving project to a more static page setup"
    This reverts commit e3357f591edf1cc0e1d4843f7944802e9925bb6f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../demo-styling.css', './app.component.css']
 })
 export class AppComponent {
 
